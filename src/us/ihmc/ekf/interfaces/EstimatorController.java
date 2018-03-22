efe26b09a76826b46ffe5121169f168197c879e6
gwiedebach <gwiedebach@ihmc.us>
2018-03-22 16:18:58 -0700

    Added state yo variables for plotting.

--- src/us/ihmc/ekf/interfaces/EstimatorController.java
+++ src/us/ihmc/ekf/interfaces/EstimatorController.java
@@ -1,8 +1,14 @@
 package us.ihmc.ekf.interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.ejml.data.DenseMatrix64F;
+
 import us.ihmc.ekf.filter.StateEstimator;
 import us.ihmc.ekf.filter.state.RobotState;
 import us.ihmc.simulationConstructionSetTools.robotController.SimpleRobotController;
+import us.ihmc.yoVariables.variable.YoDouble;
 
 public class EstimatorController extends SimpleRobotController
 {
@@ -11,12 +17,20 @@ public class EstimatorController extends SimpleRobotController
    private final StateEstimator estimator;
    private final RobotState robotState;
 
+   private final DenseMatrix64F stateVector = new DenseMatrix64F(1, 1);
+   private final List<YoDouble> yoState = new ArrayList<>();
+
    public EstimatorController(RobotSensorReader sensorReader, FullRobotModel fullRobotModel, double dt)
    {
       robotState = new RobotState(fullRobotModel, dt);
       this.sensorReader = sensorReader;
       this.fullRobotModel = fullRobotModel;
       estimator = new StateEstimator(sensorReader.getSensors(), robotState, registry);
+
+      for (int stateIdx = 0; stateIdx < robotState.getSize(); stateIdx++)
+      {
+         yoState.add(new YoDouble("x" + stateIdx, registry));
+      }
    }
 
    @Override
@@ -26,5 +40,10 @@ public class EstimatorController extends SimpleRobotController
       estimator.compute();
 
       robotState.setFullRobotModelFromState(fullRobotModel);
+      robotState.getStateVector(stateVector);
+      for (int stateIdx = 0; stateIdx < robotState.getSize(); stateIdx++)
+      {
+         yoState.get(stateIdx).set(stateVector.get(stateIdx));
+      }
    }
 }
