6adae4791f719e09e61b04ed0b4b01b03f9f7ba6
Georg Wiedebach <georg.wiedebach@gmail.com>
2020-08-03 11:28:29 -0700

    Updated native code to assert no heap allocations, use 'critical' array API.

--- nativeEKF/NativeFilterMatrixOps.cpp
+++ nativeEKF/NativeFilterMatrixOps.cpp
@@ -11,113 +11,110 @@
 
 using Eigen::MatrixXd;
 
+typedef Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>> JMatrixMap;
+
 JNIEXPORT void JNICALL Java_us_ihmc_ekf_filter_NativeFilterMatrixOpsWrapper_computeABAt(JNIEnv *env, jobject thisObj, jdoubleArray result,
-		jdoubleArray aData, jdoubleArray bData, jint n, jint m)
+      jdoubleArray aData, jdoubleArray bData, jint n, jint m)
 {
-	jdouble *aDataArray = env->GetDoubleArrayElements(aData, NULL);
-	jdouble *bDataArray = env->GetDoubleArrayElements(bData, NULL);
-	MatrixXd A = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(aDataArray, n, m);
-	MatrixXd B = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(bDataArray, m, m);
+   jdouble *aDataArray = (jdouble*) env->GetPrimitiveArrayCritical(aData, NULL);
+   jdouble *bDataArray = (jdouble*) env->GetPrimitiveArrayCritical(bData, NULL);
+   jdouble *resultDataArray = (jdouble*) env->GetPrimitiveArrayCritical(result, NULL);
 
-	MatrixXd ABAt = A * B * A.transpose();
+   JMatrixMap A(aDataArray, n, m);
+   JMatrixMap B(bDataArray, m, m);
+   JMatrixMap ABAt(resultDataArray, n, n);
 
-	jdouble *resultDataArray = new jdouble[n * n];
-	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(resultDataArray, n, n) = ABAt;
-	env->SetDoubleArrayRegion(result, 0, n * n, resultDataArray);
+   ABAt.noalias() = A * B * A.transpose();
 
-	env->ReleaseDoubleArrayElements(aData, aDataArray, 0);
-	env->ReleaseDoubleArrayElements(bData, bDataArray, 0);
-	delete resultDataArray;
+   env->ReleasePrimitiveArrayCritical(aData, aDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(bData, bDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(result, resultDataArray, 0);
 }
 
 JNIEXPORT void JNICALL Java_us_ihmc_ekf_filter_NativeFilterMatrixOpsWrapper_predictErrorCovariance
   (JNIEnv *env, jobject thisObj, jdoubleArray result, jdoubleArray fData, jdoubleArray pData, jdoubleArray qData, jint n)
 {
-	jdouble *fDataArray = env->GetDoubleArrayElements(fData, NULL);
-	jdouble *pDataArray = env->GetDoubleArrayElements(pData, NULL);
-	jdouble *qDataArray = env->GetDoubleArrayElements(qData, NULL);
-	MatrixXd F = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(fDataArray, n, n);
-	MatrixXd P = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(pDataArray, n, n);
-	MatrixXd Q = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(qDataArray, n, n);
+   jdouble *fDataArray = (jdouble*) env->GetPrimitiveArrayCritical(fData, NULL);
+   jdouble *pDataArray = (jdouble*) env->GetPrimitiveArrayCritical(pData, NULL);
+   jdouble *qDataArray = (jdouble*) env->GetPrimitiveArrayCritical(qData, NULL);
+   jdouble *resultDataArray = (jdouble*) env->GetPrimitiveArrayCritical(result, NULL);
 
-	MatrixXd Qdiag = Q.diagonal().asDiagonal();
-	MatrixXd errorCovariance = F * P.selfadjointView<Eigen::Upper>() * F.transpose() + Qdiag;
+   JMatrixMap F(fDataArray, n, n);
+   JMatrixMap P(pDataArray, n, n);
+   JMatrixMap Q(qDataArray, n, n);
+   JMatrixMap errorCovariance(resultDataArray, n, n);
 
-	jdouble *resultDataArray = new jdouble[n * n];
-	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(resultDataArray, n, n) = errorCovariance;
-	env->SetDoubleArrayRegion(result, 0, n * n, resultDataArray);
+   MatrixXd Qdiag = Q.diagonal().asDiagonal();
+   errorCovariance.noalias() = F * P.selfadjointView<Eigen::Upper>() * F.transpose() + Qdiag;
 
-	env->ReleaseDoubleArrayElements(fData, fDataArray, 0);
-	env->ReleaseDoubleArrayElements(pData, pDataArray, 0);
-	env->ReleaseDoubleArrayElements(qData, qDataArray, 0);
-	delete resultDataArray;
+   env->ReleasePrimitiveArrayCritical(fData, fDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(pData, pDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(qData, qDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(result, resultDataArray, 0);
 }
 
 JNIEXPORT void JNICALL Java_us_ihmc_ekf_filter_NativeFilterMatrixOpsWrapper_updateErrorCovariance
   (JNIEnv *env, jobject thisObj, jdoubleArray result, jdoubleArray kData, jdoubleArray hData, jdoubleArray pData, jint n, jint m)
 {
-	jdouble *kDataArray = env->GetDoubleArrayElements(kData, NULL);
-	jdouble *hDataArray = env->GetDoubleArrayElements(hData, NULL);
-	jdouble *pDataArray = env->GetDoubleArrayElements(pData, NULL);
-	MatrixXd K = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(kDataArray, m, n);
-	MatrixXd H = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(hDataArray, n, m);
-	MatrixXd P = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(pDataArray, m, m);
+   jdouble *kDataArray = (jdouble*) env->GetPrimitiveArrayCritical(kData, NULL);
+   jdouble *hDataArray = (jdouble*) env->GetPrimitiveArrayCritical(hData, NULL);
+   jdouble *pDataArray = (jdouble*) env->GetPrimitiveArrayCritical(pData, NULL);
+   jdouble *resultDataArray = (jdouble*) env->GetPrimitiveArrayCritical(result, NULL);
 
-	MatrixXd errorCovariance = (MatrixXd::Identity(m, m) - K * H) * P.selfadjointView<Eigen::Upper>();
+   JMatrixMap K(kDataArray, m, n);
+   JMatrixMap H(hDataArray, n, m);
+   JMatrixMap P(pDataArray, m, m);
+   JMatrixMap errorCovariance(resultDataArray, m, m);
 
-	jdouble *resultDataArray = new jdouble[m * m];
-	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(resultDataArray, m, m) = errorCovariance;
-	env->SetDoubleArrayRegion(result, 0, m * m, resultDataArray);
+   errorCovariance.noalias() = (MatrixXd::Identity(m, m) - K * H) * P.selfadjointView<Eigen::Upper>();
 
-	env->ReleaseDoubleArrayElements(kData, kDataArray, 0);
-	env->ReleaseDoubleArrayElements(hData, hDataArray, 0);
-	env->ReleaseDoubleArrayElements(pData, pDataArray, 0);
-	delete resultDataArray;
+   env->ReleasePrimitiveArrayCritical(kData, kDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(hData, hDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(pData, pDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(result, resultDataArray, 0);
 }
 
 JNIEXPORT void JNICALL Java_us_ihmc_ekf_filter_NativeFilterMatrixOpsWrapper_computeKalmanGain
   (JNIEnv *env, jobject thisObj, jdoubleArray result, jdoubleArray pData, jdoubleArray hData, jdoubleArray rData, jint n, jint m)
 {
-	jdouble *pDataArray = env->GetDoubleArrayElements(pData, NULL);
-	jdouble *hDataArray = env->GetDoubleArrayElements(hData, NULL);
-	jdouble *rDataArray = env->GetDoubleArrayElements(rData, NULL);
-	MatrixXd P = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(pDataArray, m, m);
-	MatrixXd H = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(hDataArray, n, m);
-	MatrixXd R = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(rDataArray, n, n);
+   jdouble *pDataArray = (jdouble*) env->GetPrimitiveArrayCritical(pData, NULL);
+   jdouble *hDataArray = (jdouble*) env->GetPrimitiveArrayCritical(hData, NULL);
+   jdouble *rDataArray = (jdouble*) env->GetPrimitiveArrayCritical(rData, NULL);
+   jdouble *resultDataArray = (jdouble*) env->GetPrimitiveArrayCritical(result, NULL);
 
-	MatrixXd PHt = P.selfadjointView<Eigen::Upper>() * H.transpose();
-	MatrixXd Rdiag = R.diagonal().asDiagonal();
-	MatrixXd toInvert = H * PHt + Rdiag;
-	MatrixXd kalmanGain = PHt * toInvert.inverse();
+   JMatrixMap P(pDataArray, m, m);
+   JMatrixMap H(hDataArray, n, m);
+   JMatrixMap R(rDataArray, n, n);
+   JMatrixMap gain(resultDataArray, m, n);
 
-	jdouble *resultDataArray = new jdouble[m * n];
-	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(resultDataArray, m, n) = kalmanGain;
-	env->SetDoubleArrayRegion(result, 0, m * n, resultDataArray);
+   MatrixXd PHt = P.selfadjointView<Eigen::Upper>() * H.transpose();
+   MatrixXd Rdiag = R.diagonal().asDiagonal();
+   MatrixXd toInvert = H * PHt + Rdiag;
+   gain.noalias() = PHt * toInvert.inverse();
 
-	env->ReleaseDoubleArrayElements(pData, pDataArray, 0);
-	env->ReleaseDoubleArrayElements(hData, hDataArray, 0);
-	env->ReleaseDoubleArrayElements(rData, rDataArray, 0);
-	delete resultDataArray;
+   env->ReleasePrimitiveArrayCritical(pData, pDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(hData, hDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(rData, rDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(result, resultDataArray, 0);
 }
 
 JNIEXPORT void JNICALL Java_us_ihmc_ekf_filter_NativeFilterMatrixOpsWrapper_updateState
   (JNIEnv *env, jobject thisObj, jdoubleArray result, jdoubleArray xData, jdoubleArray kData, jdoubleArray rData, jint n, jint m)
 {
-	jdouble *xDataArray = env->GetDoubleArrayElements(xData, NULL);
-	jdouble *kDataArray = env->GetDoubleArrayElements(kData, NULL);
-	jdouble *rDataArray = env->GetDoubleArrayElements(rData, NULL);
-	MatrixXd x = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(xDataArray, n, 1);
-	MatrixXd K = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(kDataArray, n, m);
-	MatrixXd r = Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(rDataArray, m, 1);
+   jdouble *xDataArray = (jdouble*) env->GetPrimitiveArrayCritical(xData, NULL);
+   jdouble *kDataArray = (jdouble*) env->GetPrimitiveArrayCritical(kData, NULL);
+   jdouble *rDataArray = (jdouble*) env->GetPrimitiveArrayCritical(rData, NULL);
+   jdouble *resultDataArray = (jdouble*) env->GetPrimitiveArrayCritical(result, NULL);
 
-	MatrixXd state = x + K * r;
+   JMatrixMap x(xDataArray, n, 1);
+   JMatrixMap K(kDataArray, n, m);
+   JMatrixMap r(rDataArray, m, 1);
+   JMatrixMap state(resultDataArray, n, 1);
 
-	jdouble *resultDataArray = new jdouble[n];
-	Eigen::Map<Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(resultDataArray, n, 1) = state;
-	env->SetDoubleArrayRegion(result, 0, n, resultDataArray);
+   state.noalias() = x + K * r;
 
-	env->ReleaseDoubleArrayElements(xData, xDataArray, 0);
-	env->ReleaseDoubleArrayElements(kData, kDataArray, 0);
-	env->ReleaseDoubleArrayElements(rData, rDataArray, 0);
-	delete resultDataArray;
+   env->ReleasePrimitiveArrayCritical(xData, xDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(kData, kDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(rData, rDataArray, 0);
+   env->ReleasePrimitiveArrayCritical(result, resultDataArray, 0);
 }
---
+++ nativeEKF/README.md
@@ -0,0 +1,13 @@
+# Native Filter Matrix Ops
+
+### Building:
+
+Dependency: Eigen3
+
+`mkdir build`
+
+`cd build`
+
+`cmake -DCMAKE_BUILD_TYPE=Release ..`
+
+`make`
