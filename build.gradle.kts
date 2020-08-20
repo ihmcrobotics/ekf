4edb555000a216721b21aa765f8e59008a5d98c7
Sylvain Bertrand <sbertrand@ihmc.us>
2020-08-20 11:45:05 -0500

    :arrow_up: simulation-construction-set 0.20.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,5 +40,5 @@ testDependencies {
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:simulation-construction-set:0.20.3")
+   api("us.ihmc:simulation-construction-set:0.20.4")
 }
