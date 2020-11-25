fe4f505511457f4a44a37ccc1cfa8336ba8b5938
Duncan Calvert <dcalvert@ihmc.us>
2020-11-24 22:47:23 -0600

    :arrow_up: ihmc-commons 0.30.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ mainDependencies {
    api("us.ihmc:euclid-geometry:0.15.1")
    api("us.ihmc:euclid-frame:0.15.1")
    api("us.ihmc:mecano:0.7.1")
-   api("us.ihmc:ihmc-commons:0.30.2")
+   api("us.ihmc:ihmc-commons:0.30.4")
    api("us.ihmc:ihmc-native-library-loader:1.2.1")
    api("us.ihmc:ihmc-yovariables:0.9.6")
 }
@@ -34,7 +34,7 @@ testDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("org.apache.commons:commons-math3:3.3")
 
-   api("us.ihmc:ihmc-commons-testing:0.30.2")
+   api("us.ihmc:ihmc-commons-testing:0.30.4")
 }
 
 visualizersDependencies {
