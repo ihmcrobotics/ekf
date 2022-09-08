9c2676e644e13013f4f09e35774a99d918381100
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-08 17:25:41 -0500

    :bookmark: 17-0.11.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.19.0")
    api("us.ihmc:euclid-geometry:0.19.0")
    api("us.ihmc:euclid-frame:0.19.0")
-   api("us.ihmc:mecano:0.11.2")
+   api("us.ihmc:mecano:17-0.11.3")
    api("us.ihmc:ihmc-commons:0.31.0")
    api("us.ihmc:ihmc-native-library-loader:1.3.1")
    api("us.ihmc:ihmc-yovariables:0.9.16")
