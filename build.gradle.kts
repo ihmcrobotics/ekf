8e9be0f533c53540e673e3a60cec0581243adc64
Duncan Calvert <dcalvert@ihmc.us>
2022-09-26 17:13:53 -0500

    :arrow_up: ihmc-native-library-loader 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.19.0")
    api("us.ihmc:mecano:17-0.11.5")
    api("us.ihmc:ihmc-commons:0.31.0")
-   api("us.ihmc:ihmc-native-library-loader:1.3.1")
+   api("us.ihmc:ihmc-native-library-loader:2.0.1")
    api("us.ihmc:ihmc-yovariables:0.9.16")
 }
 
