d600e1f8220a0458516c571630678f714fa8c4f4
James Foster <jfoster@ihmc.org>
2023-11-14 14:01:03 -0600

    :arrow_up: euclid 0.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,9 @@ mainDependencies {
    api("org.ejml:ejml-core:0.39")
    api("org.ejml:ejml-ddense:0.39")
 
-   api("us.ihmc:euclid:0.20.0")
-   api("us.ihmc:euclid-geometry:0.20.0")
-   api("us.ihmc:euclid-frame:0.20.0")
+   api("us.ihmc:euclid:0.21.0")
+   api("us.ihmc:euclid-geometry:0.21.0")
+   api("us.ihmc:euclid-frame:0.21.0")
    api("us.ihmc:mecano:17-0.14.0")
    api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:ihmc-native-library-loader:2.0.2")
