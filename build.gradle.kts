7f33aa8b7335a1fe2b738d4f170e5dd47523039c
Sylvain Bertrand <sbertrand@ihmc.org>
2024-02-29 16:44:57 -0600

    :arrow_up: scs 0.25.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,5 +40,5 @@ testDependencies {
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:simulation-construction-set:0.25.0")
+   api("us.ihmc:simulation-construction-set:0.25.1")
 }
