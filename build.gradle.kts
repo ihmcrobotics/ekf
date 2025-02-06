92220579f6861f151d410f851db3a7b71ad1a612
Dexton Anderson <danderson@ihmc.org>
2025-02-06 13:24:34 -0600

    :arrow_up: euclid 0.22.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,10 @@ mainDependencies {
    api("us.ihmc:euclid:0.22.3")
    api("us.ihmc:euclid-geometry:0.22.3")
    api("us.ihmc:euclid-frame:0.22.3")
-   api("us.ihmc:mecano:17-0.19.0")
+   api("us.ihmc:mecano:17-0.19.2")
    api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-native-library-loader:2.0.3")
-   api("us.ihmc:ihmc-yovariables:0.13.5")
+   api("us.ihmc:ihmc-yovariables:0.13.6")
 }
 
 testDependencies {
