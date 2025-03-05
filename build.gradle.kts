0bca13b914a0eda78877011c4db30ccdf0ace215
Dexton Anderson <danderson@ihmc.org>
2025-03-05 11:33:12 -0600

    :arrow_up: ihmc-native-library-loader 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.22.3")
    api("us.ihmc:mecano:17-0.19.2")
    api("us.ihmc:ihmc-commons:0.35.1")
-   api("us.ihmc:ihmc-native-library-loader:2.0.3")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
    api("us.ihmc:ihmc-yovariables:0.13.6")
 }
 
