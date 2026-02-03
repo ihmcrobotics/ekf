979e6afb846978204e64210e694cd883ac3accc5
Robert <rjgriffin42@gmail.com>
2026-02-02 18:12:09 -0600

    :arrow_up: mecano 17-0.19.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.22.5")
    api("us.ihmc:euclid-geometry:0.22.5")
    api("us.ihmc:euclid-frame:0.22.5")
-   api("us.ihmc:mecano:17-0.19.2")
+   api("us.ihmc:mecano:17-0.19.3")
    api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-native-library-loader:2.0.6")
    api("us.ihmc:ihmc-yovariables:0.13.7")
