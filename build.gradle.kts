02c2f50b1d8ca21137ab352c1d530b3b97d6a13c
nkitchel <nkitchel@ihmc.org>
2026-09-24 12:28:21 -0500

    :arrow_up: simulation-construction-set 0.25.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,5 +38,5 @@ testDependencies {
 visualizersDependencies {
    api(ihmc.sourceSetProject("main"))
 
-   api("us.ihmc:simulation-construction-set:0.25.4")
+   api("us.ihmc:simulation-construction-set:0.25.5")
 }
